097fd7100620bfc6f93162902b5ff0ef592c2efc
Deybson Ferreira <deybson.ferreira@iatec.com>
2022-09-09 17:17:32 -0300

    Remove ignored files

--- mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace mvc.Models;
-
-public class ErrorViewModel
-{
-    public string RequestId { get; set; }
-
-    public string Title { get; set; }
-    public string Message { get; set; }
-}
